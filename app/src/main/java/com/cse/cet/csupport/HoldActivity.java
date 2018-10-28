af3365c523052dc6084bdf04d3f65e4502096a23
JV <joycevarg@gmail.com>
2018-10-28 16:28:49 +0530

    Script added

---
+++ app/src/main/java/com/cse/cet/csupport/HoldActivity.java
@@ -0,0 +1,37 @@
+package com.cse.cet.csupport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_splash);
+
+       // getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+     //   getActionBar().hide();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        }, 2000);
+    }
+}
